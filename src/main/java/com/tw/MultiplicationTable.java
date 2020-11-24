201e22cde831aceea7dd91de2f913e707e0684f5
AUAL <alex.au@oocl.com>
2020-11-24 16:39:27 +0800

    [multiplication_table][documentation] Alex Au : add estimated time to "D"

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
 
 
     //    P: in 5 minute
-    //    D:
+    //    D: ~5min
     //    C: the requirement need %n which was CRLF, took some time to check this
     //    A: be more familiar with Java string operations and different formatting methods
     public String create(int startNumber, int endNumber) {
@@ -21,7 +21,7 @@ public class MultiplicationTable {
     }
 
     //    P: in one minute
-    //    D:
+    //    D: <1min
     //    C: N/A
     //    A: N/A
     public Boolean isStartNumLeqEndNum(int startNumber, int endNumber) {
@@ -29,7 +29,7 @@ public class MultiplicationTable {
     }
 
     //    P: in one minute
-    //    D:
+    //    D: <1min
     //    C: N/A
     //    A: N/A
     public Boolean isInValidRange(int startNumber, int endNumber) {
@@ -37,7 +37,7 @@ public class MultiplicationTable {
     }
 
     //    P: in 5 minute with child function created
-    //    D:
+    //    D: ~5min
     //    C: missed an necessary input when drafting the chart, the line would need the start number as it doesn't always start from 1?
     //    A: should have dry-run the input -> output logic in mind when drafting the graph
     public String generateMultiplicationTableLine(int startNumber, int endNumber, int commonMultiplier) {
@@ -52,7 +52,7 @@ public class MultiplicationTable {
 
 
     //    P: in 2 minute
-    //    D:
+    //    D: <1min
     //    C: N/A
     //    A: N/A
     public String generateMultiplicationTableItem(int multiplicand, int multiplier) {
